e63d7ead75f2e10a156dd724b402fc827c627f92
wurbanski-del <w.urbanski@escola.pr.gov.br>
2026-04-01 15:15:31 +0000

    sauvar

---
+++ conversoes.js
@@ -0,0 +1,13 @@
+ // tipo de dados 
+ //booleanos
+
+ //conversão implicita
+
+ const numero = 456;
+ const numeroString = '456';
+
+ console.(numero == numeroString);
+
+ //conversão
+
+ console.log(numero + Number)(numeroString);
--- falso-verdadeiro.js
+++ falso-verdadeiro.js
@@ -1,4 +1,24 @@
-const usuarioologado = true;
-const contapaga = false;
+//const usuariologado = true;
+//const contapaga = false;
 
- //0
+//0 ==> false
+// 1 ==> true
+
+//console.log(0 == false);
+//console.log('' == false);
+//console.log(1 == true);
+
+//mu11
+//undefined
+
+let minhavar
+let varNull = null
+
+console.log(minhaVar)
+console.log(varNull)
+
+let numero = 3;
+let texto = altura';
+
+console.log(typeof numero)
+console.log(typeof texto);
